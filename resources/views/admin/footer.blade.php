fe3df64adef65ed002c175ff952a36ad978126d9
小A <wanga503@outlook.com>
2017-07-20 16:39:42 +0800

    后台footer

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
 				<span class="footer-brand">
-					<strong class="text-danger">O.M.P.</strong> Admin
+					Admin
 				</span>
     <p class="no-margin">
         &copy; 2017 <strong>Alexa</strong>. ALL Rights Reserved.
