d4a51b054f9147dc65d931daad0df43830af350b
小A <wanga503@outlook.com>
2017-08-17 12:18:10 +0800

    优化积分更新代码

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -186,6 +186,9 @@ class OwnerController extends Controller
     public function update()
     {
         $uid = session('discuz.user.uid');
+        if( null == $uid ){
+            return;
+        }
         $verifies = \App\Verify::where('uid',$uid)->where('status','>=',0)->get();
         $updated_at = session('points.updated_at');
         //限制用户频繁请求
@@ -195,73 +198,7 @@ class OwnerController extends Controller
         \Session::put('points.updated_at', time());
 
         foreach($verifies as $verify){
-            $frame_number = $verify->frame_number;//车架号
-
-            $_log = \App\OwnerLog::where('verify_id', $verify->id)->where('generate_way',1)->orderBy('score_id','DESC')->first();
-            if( $_log == null ){
-                $score_id = 0;
-            }
-            else{
-                $score_id = $_log->score_id;
-            }
-            //新增积分
-            $client = new \SoapClient("http://124.162.32.6:8081/infodms_interface_hy/services/HY02SOAP?wsdl");
-            $options = [
-                'in'=>json_encode([
-                    'frame_number'=>$frame_number,
-                    'score_id' => $score_id,
-                ])
-            ];
-            $response = $client->__soapCall("queryPartsInfo", array($options));
-            $result = json_decode($response->out,true);
-            //var_dump($result);
-            if($result && $result['ret'] == 0 && isset($result['data']) && is_array($result['data'])){
-                foreach ($result['data'] as $data){
-                    $count = \App\OwnerLog::where('score_id', $data['SCORE_ID'])->count();
-                    if( $count > 0 ){
-                        continue;
-                    }
-                    $data['title'] = '车主奖励';
-                    $data['generate_way'] = 1;
-                    $data['verify_id'] = $verify->id;
-                    \App\Helpers\Helper::updateLog($uid,$data);
-                }
-            }
-
-            //工单取消
-            $_log = \App\OwnerLog::where('verify_id', $verify->id)->where('generate_way',2)->orderBy('score_id','DESC')->first();
-            if( $_log == null ){
-                $score_id = 0;
-            }
-            else{
-                $score_id = $_log->score_id;
-            }
-            $client = new \SoapClient("http://124.162.32.6:8081/infodms_interface_hy/services/HY05SOAP?wsdl");
-            $options = [
-                'in'=>json_encode([
-                    'frame_number'=>$frame_number,
-                    'score_id' => $score_id,
-                ])
-            ];
-            $response = $client->__soapCall("CancelOrderAccount", array($options));
-            $result1 = json_decode($response->out,true);
-            //var_dump($result1);
-            if($result1 && $result1['ret'] == 0 && isset($result1['data']) && is_array($result1['data'])){
-                foreach ($result1['data'] as $data){
-                    $count = \App\OwnerLog::where('score_id', $data['SCORE_ID'])->count();
-
-                    if( $count > 0 ){
-                        continue;
-                    }
-                    $data['title'] = '车主工单取消';
-                    $data['generate_way'] = 2;
-                    $data['verify_id'] = $verify->id;
-                    \App\Helpers\Helper::updateLog($uid,$data);
-                    \App\OwnerLog::where('rono', $data['Rono'])->where('generate_way','1')->delete();
-                }
-            }
-            //$verify->status = 1;
-            //$verify->save();
+            \App\Helpers\Helper::pointsUpdate($verify);
         }
         return response('',200);
     }
