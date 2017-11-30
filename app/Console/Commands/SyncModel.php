4c337db9a1ffbfa0c6bdbca98f9bdfa302dc5185
小A <wanga503@outlook.com>
2017-11-30 22:32:30 +0800

    优化同步脚本

--- app/Console/Commands/SyncModel.php
+++ app/Console/Commands/SyncModel.php
@@ -11,7 +11,7 @@ class SyncModel extends Command
     *
     * @var string
     */
-    protected $signature = 'sync:model';
+    protected $signature = 'sync:model {n?}';
 
     /**
     * The console command description.
@@ -37,31 +37,30 @@ class SyncModel extends Command
     */
     public function handle()
     {
-        $count = \App\Verify::where('status','>=',0)->count();
-        $n = ceil($count/1000);
-        for ($i=0; $i < $n; $i++) {
-            $verifies = \App\Verify::where('status','>=',0)->skip($i*1000)->take(1000)->get();
-            foreach($verifies as $verify){
-                $options = [
-                    'frame_number'=>substr($verify->frame_number,-8),
-                    'id_card'=>$verify->id_card,
-                    'register_date'=>date('Y-m-d H:i:s'),
-                    'type'=>'1',
-                ];
+        $n =  null == $this->argument('n') ? 1 :  $this->argument('n');
+        //$count = \App\Verify::where('status','>=',0)->count();
+        //$total = ceil($count/1000);
+        $verifies = \App\Verify::where('status','>=',0)->skip(($n-1)*10000)->take(10000)->get();
+        foreach($verifies as $verify){
+            $options = [
+                'frame_number'=>substr($verify->frame_number,-8),
+                'id_card'=>$verify->id_card,
+                'register_date'=>date('Y-m-d H:i:s'),
+                'type'=>'1',
+            ];
 
-                $client = new \SoapClient("http://124.162.32.6:8081/infodms_interface_hy/services/HY01SOAP?wsdl");
-                $options = [
-                    'in'=>json_encode($options),
-                ];
-                $response = $client->__soapCall("Hy01", array($options));
-                $result = json_decode($response->out,true);
-                if( $result && $result['ret'] == 0){
-                    $model = \App\Helpers\Helper::replaceCarModel($result['modelCode']);
-                    $this->info($verify->id.','.$model.','.$verify->model_code);
-                    if($model != $verify->model_code){
-                        $verify->model_code = $model;
-                        $verify->save();
-                    }
+            $client = new \SoapClient("http://124.162.32.6:8081/infodms_interface_hy/services/HY01SOAP?wsdl");
+            $options = [
+                'in'=>json_encode($options),
+            ];
+            $response = $client->__soapCall("Hy01", array($options));
+            $result = json_decode($response->out,true);
+            if( $result && $result['ret'] == 0){
+                $model = \App\Helpers\Helper::replaceCarModel($result['modelCode']);
+                $this->info($verify->id.','.$model.','.$verify->model_code.','.$result['modelCode']);
+                if($model != $verify->model_code){
+                    $verify->model_code = $model;
+                    $verify->save();
                 }
             }
         }
