e3a5f6f781785f2dfea7e2764755ed59afe97f48
小A <wanga503@outlook.com>
2018-04-03 15:18:06 +0800

    批量更新车型

--- app/Console/Commands/SyncModel.php
+++ app/Console/Commands/SyncModel.php
@@ -40,31 +40,55 @@ class SyncModel extends Command
         $n =  null == $this->argument('n') ? 1 :  $this->argument('n');
         //$count = \App\Verify::where('status','>=',0)->count();
         //$total = ceil($count/1000);
-        $verifies = \App\Verify::where('status','>=',0)->skip(($n-1)*10000)->take(10000)->get();
+        $verifies = \App\Verify::where('status','>=',0)
+            ->where('created_at','>=','2018-03-01')
+            //->where('id','375099')
+            ->skip(($n-1)*10000)->take(10000)->get();
         foreach($verifies as $verify){
+            $this->info($verify->id);
             $options = [
                 'frame_number'=>substr($verify->frame_number,-8),
                 'id_card'=>$verify->id_card,
                 'register_date'=>date('Y-m-d H:i:s'),
                 'type'=>'1',
             ];
-
-            $client = new \SoapClient("http://124.162.32.6:8081/infodms_interface_hy/services/HY01SOAP?wsdl");
-            $options = [
-                'in'=>json_encode($options),
-            ];
-            $response = $client->__soapCall("Hy01", array($options));
-            $result = json_decode($response->out,true);
+            try{
+                $client = new \SoapClient("http://124.162.32.6:8081/infodms_interface_hy/services/HY01SOAP?wsdl");
+                $options = [
+                    'in'=>json_encode($options),
+                ];
+                $response = $client->__soapCall("Hy01", array($options));
+                $result = json_decode($response->out,true);
+            }catch (\SoapFault $fault) {
+                $result = false;
+                $this->info('网络错误');
+            }
+            
             if( $result && $result['ret'] == 0){
                 $model = \App\Helpers\Helper::replaceCarModel($result['modelCode']);
+                $this->info($verify->id.','.$verify->frame_number.','.$model.','.$verify->model_code.','.$result['modelCode']);
                 if($model != $verify->model_code){
-                    $this->info($verify->id.','.$model.','.$verify->model_code.','.$result['modelCode']);
+                    $credit1 = \App\Helpers\Helper::getCreditsFromCarModel($model);
+                    $credit2 = \App\Helpers\Helper::getCreditsFromCarModel($verify->model_code);
                     $verify->model_code = $model;
                     $verify->save();
+
+                    $user_count = \App\UserCount::where('uid',$verify->uid)->first();
+                    if( $user_count != null ){
+                        $user_count->extcredits1 += $credit2 - $credits1;
+                        //$user_count->extcredits4 += $credits4;
+                        //更新积分
+                        \DB::table('discuz_common_member_count')->where('uid',$verify->uid)->update([
+                            'extcredits1' => $user_count->extcredits1,
+                            //'extcredits4' => $user_count->extcredits4,
+                        ]);
+                        $this->info($verify->id.','.$model.','.$verify->model_code.','.$result['modelCode'].',更改前积分:'.$credit1.',更改后:'.$credit2.',最终积分:'. $user_count->extcredits1);
+                    }
+                    
                 }
             }
         }
-        if(null != $verifies){
+        if(null != $verifies || count($verifies) != 0){
             $this->call('sync:model', ['n'=>$n+1]);
         }
     }
