f7680603c5365472d82630b5a5ad21285f7e3ef2
小A <wanga503@outlook.com>
2018-04-20 16:43:39 +0800

    菜单调整

--- resources/views/mall/mobile/car_bar.blade.php
+++ resources/views/mall/mobile/car_bar.blade.php
@@ -4,12 +4,9 @@
             <li class="{{ $active == 'mall' ? 'active':'' }}">
                 <a href="/mall"><i class="icon-bar icon-cart"></i><span>礼品兑换</span></a>
             </li>
-            <li class="{{ $active == 'activity' ? 'active':'' }}">
-                <a href="/bbs/forum.php?gid=44"><i class="icon-bar icon-activity"></i><span>最新活动</span></a>
-            </li>
-            <!--<li class="{{ $active == 'club' ? 'active':'' }}">
+            <li class="{{ $active == 'club' ? 'active':'' }}">
                 <a href="/bbs/forum.php"><i class="icon-bar icon-club"></i><span>风迷社区</span></a>
-            </li>-->
+            </li>
             <li class="{{ $active == 'profile' ? 'active':'' }}">
                 <a href="/bbs/home.php?mod=space&uid=1&do=profile&mycenter=1&mobile=2"><i class="icon-bar icon-profile"></i><span>个人中心</span></a>
             </li>
